d60d4081eba03bc5c59d260c3248699946a78ab5
celestine-021 <wangaricelestine036@gmail.comcla>
2026-09-22 20:26:21 +0300

    Fix pizza menu app entry point

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <main>
+      <h1>Hello React!</h1>
+      {pizzaData.map((pizza) => (
+        <Pizza key={pizza.name} pizza={pizza} />
+      ))}
+    </main>
+  );
+}
+
+function Pizza({ pizza }) {
+  return (
+    <article>
+      <h2>{pizza.name}</h2>
+      <img src={`/${pizza.photoName}`} alt={pizza.name} width="200" />
+      <p>{pizza.ingredients}</p>
+      <strong>{pizza.soldOut ? "Sold out" : `$${pizza.price}`}</strong>
+    </article>
+  );
+}
+
+export default App;
